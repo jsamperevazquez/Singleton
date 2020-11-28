3ca27691cda6c55930f2b63438fa9ce032d9d578
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2020-11-28 09:54:47 +0100

    Inicio

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * Creado por @autor: angel
+ * El  28 de nov. de 2020.
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Proyecto para aprender a hacer un Singleton");
+    }
+}
