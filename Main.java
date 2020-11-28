8c5de41aafcd5111af4b1fac8dfd8f1a0a72c538
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2020-11-28 10:04:23 +0100

    Constructor a private, error en la clase principal

--- Main.java
+++ Main.java
@@ -5,5 +5,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto para aprender a hacer un Singleton");
+
+
+        //Intento instanciar un objeto con el constructor privado
+        Singleton miUnicaInstancia = new Singleton();
+
     }
 }
---
+++ Singleton.java
@@ -0,0 +1,9 @@
+/**
+ * Creado por @autor: angel
+ * El  28 de nov. de 2020.
+ * Solo una instancia
+ **/
+
+public class Singleton {
+    private  Singleton() {}
+}
